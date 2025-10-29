2e26907b85f64049406d7a7a9b414e9c1abb042f
atiq1512 <atiqnurnatasha15@gmail.com>
2025-10-29 18:27:46 +0800

    Update tsp.py

--- tsp.py
+++ tsp.py
@@ -6,6 +6,7 @@ import numpy as np
 import statistics
 import pandas as pd
 import seaborn as sns
+
 x = [0,3,6,7,15,10,16,5,8,1.5]
 y = [1,2,1,4.5,-1,2.5,11,6,9,12]
 cities_names = ["Gliwice", "Cairo", "Rome", "Krakow", "Paris", "Alexandria", "Berlin", "Tokyo", "Rio", "Budapest"]
@@ -14,6 +15,7 @@ n_population = 250
 crossover_per = 0.8
 mutation_per = 0.2
 n_generations = 200
+
 # Pastel Pallete
 colors = sns.color_palette("pastel", len(cities_names))
 
@@ -47,10 +49,11 @@ for i, (city, (city_x, city_y)) in enumerate(city_coords.items()):
     for j, (other_city, (other_x, other_y)) in enumerate(city_coords.items()):
         if i != j:
             ax.plot([city_x, other_x], [city_y, other_y], color='gray', linestyle='-', linewidth=1, alpha=0.1)
+
 fig.set_size_inches(16, 12)
 plt.show()
-#population
 
+#population
 def initial_population(cities_list, n_population = 250):
 
     """
@@ -71,13 +74,15 @@ def initial_population(cities_list, n_population = 250):
         population_perms.append(list(possible_perms[i]))
 
     return population_perms
-  #distance between two cities
+
+#distance between two cities
 
 def dist_two_cities(city_1, city_2):
 
     city_1_coords = city_coords[city_1]
     city_2_coords = city_coords[city_2]
     return np.sqrt(np.sum((np.array(city_1_coords) - np.array(city_2_coords))**2))
+
 def total_dist_individual(individual):
 
     total_dist = 0
@@ -87,7 +92,8 @@ def total_dist_individual(individual):
         else:
             total_dist += dist_two_cities(individual[i], individual[i+1])
     return total_dist
-    #fitness probablity function
+
+#fitness probablity function
 
 def fitness_prob(population):
     """
@@ -106,7 +112,8 @@ def fitness_prob(population):
     population_fitness_sum = sum(population_fitness)
     population_fitness_probs = population_fitness / population_fitness_sum
     return population_fitness_probs
-  #roulette wheel
+
+#roulette wheel
 
 def roulette_wheel(population, fitness_probs):
     """
@@ -121,7 +128,8 @@ def roulette_wheel(population, fitness_probs):
     bool_prob_array = population_fitness_probs_cumsum < np.random.uniform(0,1,1)
     selected_individual_index = len(bool_prob_array[bool_prob_array == True]) - 1
     return population[selected_individual_index]
-  #crossover
+
+#crossover
 
 def crossover(parent_1, parent_2):
     """
@@ -147,7 +155,8 @@ def crossover(parent_1, parent_2):
 
 
     return offspring_1, offspring_2
-  #mutation
+
+#mutation
 
 def mutation(offspring):
     """
@@ -165,6 +174,7 @@ def mutation(offspring):
     offspring[index_1] = offspring[index_2]
     offspring[index_2] = temp
     return(offspring)
+
 def run_ga(cities_names, n_population, n_generations, crossover_per, mutation_per):
 
     population = initial_population(cities_names, n_population)
@@ -249,17 +259,23 @@ def run_ga(cities_names, n_population, n_generations, crossover_per, mutation_pe
         random.shuffle(best_mixed_offspring)
 
     return best_mixed_offspring
+
 best_mixed_offspring = run_ga(cities_names, n_population, n_generations, crossover_per, mutation_per)
+
 total_dist_all_individuals = []
 for i in range(0, n_population):
     total_dist_all_individuals.append(total_dist_individual(best_mixed_offspring[i]))
-  index_minimum = np.argmin(total_dist_all_individuals)
+
+index_minimum = np.argmin(total_dist_all_individuals)
+
 minimum_distance = min(total_dist_all_individuals)
 minimum_distance
+
 #shortest path
 # shortest_path = offspring_list[index_minimum]
 shortest_path = best_mixed_offspring[index_minimum]
 shortest_path
+
 x_shortest = []
 y_shortest = []
 for city in shortest_path:
@@ -269,6 +285,7 @@ for city in shortest_path:
 
 x_shortest.append(x_shortest[0])
 y_shortest.append(y_shortest[0])
+
 fig, ax = plt.subplots()
 ax.plot(x_shortest, y_shortest, '--go', label='Best Route', linewidth=2.5)
 plt.legend()

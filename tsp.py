a17af71dfdfce2750d5f886cab6648e356bd378c
atiq1512 <atiqnurnatasha15@gmail.com>
2025-10-29 18:25:07 +0800

    Update tsp.py

--- tsp.py
+++ tsp.py
@@ -47,10 +47,10 @@ for i, (city, (city_x, city_y)) in enumerate(city_coords.items()):
     for j, (other_city, (other_x, other_y)) in enumerate(city_coords.items()):
         if i != j:
             ax.plot([city_x, other_x], [city_y, other_y], color='gray', linestyle='-', linewidth=1, alpha=0.1)
-
 fig.set_size_inches(16, 12)
 plt.show()
 #population
+
 def initial_population(cities_list, n_population = 250):
 
     """

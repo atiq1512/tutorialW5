4126b6d07c6d58738446e4a4cf54365520820f8e
atiq1512 <atiqnurnatasha15@gmail.com>
2025-10-29 18:13:01 +0800

    Update ga.py

--- ga.py
+++ ga.py
@@ -7,7 +7,7 @@ POP_SIZE = 500
 MUT_RATE = 0.2
 
 #TARGET: Our goal.
-TARGET = 'fakhitah'
+TARGET = 'natasha'
 
 #GENES: Options from which our population would be created.
 GENES = ' abcdefghijklmnopqrstuvwxyz'
@@ -111,9 +111,9 @@ def main(POP_SIZE, MUT_RATE, TARGET, GENES):
 
 
       if (population[0][1] == 0):
-        print('Target found')
-        print('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
+        st.write('Target found')
+        st.write('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
         break
-      print('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
+      st.write('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
       generation+=1
 result = main(POP_SIZE, MUT_RATE, TARGET, GENES)
